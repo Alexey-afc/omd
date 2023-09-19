cf320f87622aee145b603b40f5c9865884ac7c2b
Alexey <55971755+Alexey-afc@users.noreply.github.com>
2023-09-19 16:09:13 +0300

    Home

---
+++ omd.py
@@ -0,0 +1,22 @@
+def step2_umbrella():
+    print("Странно,ведь утки немокнут под дождем")
+def step2_no_umbrella():
+    print("Логично, ведь чем она будет держать?")
+def step1():
+    print(
+        'Утка-маляр 🦆 решила выпить зайти в бар. '
+        'Взять ей зонтик? ☂️'
+    )
+    option = ''
+    options = {'да': True, 'нет': False}
+    while option not in options:
+        print('Выберите: {}/{}'.format(*options))
+        option = input()
+
+    if options[option]:
+        return step2_umbrella()
+    return step2_no_umbrella()
+
+
+if __name__ == '__main__':
+    step1()
